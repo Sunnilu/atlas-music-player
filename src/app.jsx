15df6eca93ec693d24480304ba0c0c98b7d8a3b0
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-22 18:36:14 -0500

    added script for tailwind setup

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+export default function App() {
+    return (
+      <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">
+          Tailwind Setup Complete!
+        </h1>
+      </div>
+    );
+  }
+  
