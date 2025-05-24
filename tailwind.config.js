7a5bd968c1e8bfe7ba3d38d4504f741522523592
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-24 14:39:04 -0500

    added to tailwind spin-slow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 // tailwind.config.js
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'spin-slow': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'spin-slow': 'spin-slow 4s linear infinite',
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
