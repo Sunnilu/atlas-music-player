6539226a054b820e92a41343cea4fdabffe77e87
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-24 13:33:14 -0500

    added components to music player

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,18 @@
+// src/components/MusicPlayer.jsx
+import CoverArt from './CoverArt';
+import SongTitle from './SongTitle';
+import PlayControls from './PlayControls';
+import VolumeControls from './VolumeControls';
+import PlayListItem from './PlayListItem';
+
 export default function MusicPlayer() {
-  return <p>Music player goes here</p>;
+  return (
+    <div className="max-w-md mx-auto p-6 bg-white rounded-xl shadow-lg">
+      <CoverArt />
+      <SongTitle />
+      <PlayControls />
+      <VolumeControls />
+      <PlayListItem />
+    </div>
+  );
 }
