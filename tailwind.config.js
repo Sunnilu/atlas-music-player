9f96b3e4877f6fc2d2e06a20f3cfc596e615d002
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-24 20:16:03 -0500

    spin slow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        custom: '0px 4px 4px 0px #D5D7D840',
+      },
       keyframes: {
         'spin-slow': {
           '0%': { transform: 'rotate(0deg)' },
