d3091b07dc27b5bc7776ae77f39b1cdf496b63c9
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-25 20:42:00 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="text-center py-6 text-sm text-gray-400">
-    &copy; 2024 Atlas School
+  <footer className="text-center py-6 text-sm text-text-light font-custom opacity-60">
+    &copy; 2024 Atlas School. All rights reserved.
   </footer>
 );
 
