706ef82cded3277a5a9bd9f1906f72b76b07c648
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-25 20:32:48 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#1A73E8',     // Blue
+        accent: '#F35A69',      // Coral
+        'bg-dark': '#1E1E2F',   // Deep navy
+        'text-light': '#F5F5F5',
+        'surface': '#292942'
+      },
+      borderRadius: {
+        'xl': '1.25rem',
+        '2xl': '1.5rem'
+      },
+      fontFamily: {
+        custom: ['"Poppins"', 'sans-serif']
+      },
       boxShadow: {
         custom: '0px 4px 4px 0px #D5D7D840',
-      },
-      keyframes: {
-        'spin-slow': {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
-        },
-      },
-      animation: {
-        'spin-slow': 'spin-slow 4s linear infinite',
-      },
+      }
     },
   },
   plugins: [],
-};
+}
