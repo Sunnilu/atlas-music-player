3dc34e1b3643add0aa493ad94a70d67029d0a5a3
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-26 08:54:26 -0500

    types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// src/types.ts
+
+export interface Song {
+    id: number;
+    title: string;
+    artist: string;
+    duration: string;
+    // Add more fields here if needed from the API (e.g., url, albumArt, etc.)
+  }
+  
