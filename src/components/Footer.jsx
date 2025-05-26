919012ca526645fcde5b0402f7aff18fb84e5ef6
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-26 08:13:12 -0500

    applied dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="text-center py-6 text-sm text-text-light font-custom opacity-60">
+  <footer className="text-center py-6 text-sm font-custom bg-white text-gray-700 dark:bg-bg-dark dark:text-text-light border-t border-gray-200 dark:border-white/10">
     &copy; 2024 Atlas School. All rights reserved.
   </footer>
 );
