72883a42dae7ab39aa544ba76aa9aad955a24dbc
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-26 20:42:25 -0500

    missing song=song

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,27 +1,19 @@
 // src/components/Playlist.tsx
 import React from 'react';
 import PlayListItem from '@components/PlayListItem';
-interface Song {
-  id: number;
-  title: string;
-  artist: string;
-  duration: number;
-  // Add other fields as necessary
-}
+import { Song } from '@types';
 
 interface PlaylistProps {
   songs: Song[];
-  selectedSongId: number | undefined;
+  selectedSongId?: number;
   onSelectSong: (song: Song) => void;
 }
 
 const Playlist: React.FC<PlaylistProps> = ({ songs, selectedSongId, onSelectSong }) => {
   return (
     <div className="w-full h-full font-custom text-gray-900 dark:text-text-light">
-      <h2 className="text-xl font-semibold text-primary dark:text-text-light px-2 pb-2">
-        Playlist
-      </h2>
-      <div className="rounded-xl bg-white dark:bg-surface overflow-y-auto max-h-full space-y-2 p-2 transition-all">
+      <h2 className="text-xl font-semibold text-primary px-2 pb-2">Playlist</h2>
+      <div className="rounded-xl bg-surface overflow-y-auto max-h-full space-y-2 p-2">
         {songs.map((song) => (
           <PlayListItem
             key={song.id}
