d8fd540dd79b435ce45b53737b3aa6495536b716
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-27 20:27:46 -0500

    changed imprt song from @types also added songs loaded: songs length

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,7 +1,8 @@
 // src/components/Playlist.tsx
 import React from 'react';
 import PlayListItem from '@components/PlayListItem';
-import { Song } from '@types';
+import { Song } from '../types'; // plain relative import
+
 
 interface PlaylistProps {
   songs: Song[];
@@ -13,7 +14,10 @@ const Playlist: React.FC<PlaylistProps> = ({ songs, selectedSongId, onSelectSong
   return (
     <div className="w-full h-full font-custom text-gray-900 dark:text-text-light">
       <h2 className="text-xl font-semibold text-primary px-2 pb-2">Playlist</h2>
-      <p className="text-sm text-red-500 px-2">Songs loaded: {songs.length}</p>
+      <p className="text-sm text-center text-gray-500 mb-2">
+        Songs loaded: {songs.length}
+      </p>
+
       <div className="rounded-xl bg-surface overflow-y-auto max-h-full space-y-2 p-2">
         {songs.map((song) => (
           <PlayListItem
