a86f058ba67029506ed5c98ff20288d99a3ead74
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-30 13:03:40 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -7,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
-      '@types': path.resolve(__dirname, './src/types.ts')
+      '@types': path.resolve(__dirname, './src/types')
     }
   }
 });
