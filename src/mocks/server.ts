585cbf2bbe55e7e5d322ebc1f55e319ba0685118
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-31 20:37:04 -0500

    updates

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -3,3 +3,8 @@ import { setupServer } from 'msw/node';
 import { handlers } from './handlers';
 
 export const server = setupServer(...handlers);
+
+// Optional: logs request start for debugging
+server.events.on('request:start', ({ request }) => {
+  console.log(`[MSW] Intercepting request: ${request.method} ${request.url}`);
+});
