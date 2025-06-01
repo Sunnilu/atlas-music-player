333bff35e0f7df5a672e46b17ddd1a071c98957b
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-31 19:41:11 -0500

    updating handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/api/v1/playlist', (req, res, ctx) => {
+  rest.get('/api/v1/playlist', (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json([
