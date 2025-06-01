fa799d32232fcecda4e2503fb3b3077ee5642625
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-01 06:01:33 -0500

    updated

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,14 @@
-import '@testing-library/jest-dom';
+// src/setupTests.ts
 import { server } from '../src/mocks/server';
 import { afterAll, afterEach, beforeAll } from 'vitest';
 
-beforeAll(() => server.listen());
+// Start server before all tests
+beforeAll(() => server.listen({
+  onUnhandledRequest: 'error'
+}));
+
+// Reset handlers after each test
 afterEach(() => server.resetHandlers());
+
+// Close server after all tests
 afterAll(() => server.close());
