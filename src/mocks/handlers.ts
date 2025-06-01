4e5bde5214a215d116d7c0c66c49b881900bf5b7
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-31 19:12:03 -0500

    updated

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,8 @@
 // src/mocks/handlers.ts
-import { rest } from 'msw';
+import { rest } from 'msw/node';
 
 export const handlers = [
-  rest.get('/api/v1/playlist', (req, res, ctx) => {
+  rest.get('/api/v1/playlist', (_req: any, res: (arg0: any, arg1: any) => any, ctx: { status: (arg0: number) => any; json: (arg0: { id: number; title: string; artist: string; audio: string; image: string; duration: string; lyrics: string; }[]) => any; }) => {
     return res(
       ctx.status(200),
       ctx.json([
